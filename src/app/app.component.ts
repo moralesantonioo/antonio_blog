2c0a6b06dbd0cfc1a47817b0ec03ba079825eb08
Antonio.morales <antonio.morales@gestionysistemas.com>
2023-11-13 23:59:15 -0500

    correcciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isDarkThemeActive: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) { }
+
+  ngAfterViewInit() {
+    const boton = this.document.querySelector('#btn1');
+
+    if (boton instanceof HTMLElement) {
+      this.renderer.listen(boton, 'mouseover', () => {
+        this.handleMouseOver(boton);
+      });
+    }
+  }
+
+  private handleMouseOver(boton: HTMLElement) {
+    if (boton.classList.contains('posicion_normal')) {
+      this.renderer.removeClass(boton, 'posicion_normal');
+      this.renderer.addClass(boton, 'move1');
+    } else if (boton.classList.contains('move1')) {
+      this.renderer.removeClass(boton, 'move1');
+      this.renderer.addClass(boton, 'move2');
+    } else if (boton.classList.contains('move2')) {
+      this.renderer.removeClass(boton, 'move2');
+      this.renderer.addClass(boton, 'move3');
+    } else if (boton.classList.contains('move3')) {
+      this.renderer.removeClass(boton, 'move3');
+      this.renderer.addClass(boton, 'posicion_normal');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,7 +20,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
